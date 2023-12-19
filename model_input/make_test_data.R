2c1c50d52de8c6a2f87ad4ad648d1755bf5c4d63
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2023-12-19 00:39:50 -0400

    draft of inference config

---
+++ model_input/make_test_data.R
@@ -0,0 +1,14 @@
+# Script to take output from a model simulated with another config and turn it into fake "data" that can be used as ground truth input to a different inference config file
+
+# config to take output from
+
+# config that will run inference
+
+# output location
+
+# outcomes variables to choose
+
+# filter these outcome variables for desired dates then aggregate to desired level
+
+
+# output to correct file form
